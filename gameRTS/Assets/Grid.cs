c4677c28ebd01a4ccc770229e7efd4eb18667e45
LastlightinDarkness <Johnson.T.Nathaniel@gmail.com>
2018-02-02 09:56:20 -0900

    Grid working on it poorly

---
+++ gameRTS/Assets/Grid.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(MeshFilter), typeof (MeshRenderer))]
+public class Grid : MonoBehaviour {
+
+	public int xSize, ySize;
+
+	}
+	
