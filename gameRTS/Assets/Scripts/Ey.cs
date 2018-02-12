ed8aeea5e7d0cd0d12f84a15b64ef416d869c7bd
dos54 <dragonofshame@gmail.com>
2018-02-12 09:07:28 -0900

    sdfgsdfgsdf
    gsdfgdsfg

---
+++ gameRTS/Assets/Scripts/Ey.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Ey : MonoBehaviour
+{
+	public Button Button;
+	public Text Text;
+
+	void Start()
+	{
+		Button btn = Button.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		Debug.Log("You have clicked the button!");
+		Button.GetComponentInChildren<Text>().cachedTextGenerator;
+
+	}
+}
