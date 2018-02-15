61ee01b8f9b01fd58add1855c2561965b617ab01
dos54 <dragonofshame@gmail.com>
2018-02-15 09:09:47 -0900

    Just Stuff
    a

---
+++ projectSandwich/Assets/scripts/Reeeeeeeee.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class Reeeeeeeee : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		Transform tran = GetComponent<Transform> ();
+		tran.Translate (Vector2.up * 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
