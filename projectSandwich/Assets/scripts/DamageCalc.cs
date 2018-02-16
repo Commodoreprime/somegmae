2bd872a5c8fff754639931588603cf22ef8426ce
LastlightinDarkness <Johnson.T.Nathaniel@gmail.com>
2018-02-16 09:40:23 -0900

    AI movement added to all models
    Don't delete my pyramid or you break everything.

---
+++ projectSandwich/Assets/scripts/DamageCalc.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+if(Input.GetMouseButtonDown(0))
+{
+	if(Bullet)
+	{
+		GameObject go = Instantiate(prefabBullet);
+		Bullet bu = go.GetComponent<Bullet>();
+		if(bu)
+		{
+			Vector2 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			bu.Shoot(rb2D.position, target, DamageType, DamageAmount);
+		}
+	}
+}
