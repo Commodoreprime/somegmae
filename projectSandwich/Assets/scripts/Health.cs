66bfb609583ade6d0d0b6bbc5f186b0190d91854
LastlightinDarkness <Johnson.T.Nathaniel@gmail.com>
2018-03-02 09:49:49 -0900

    health for captain meow somewhat done

---
+++ projectSandwich/Assets/scripts/Health.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour 
+{
+	public int maxHealth;
+
+	void Update(){
+		
+	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag == "Bullet") {
+			maxHealth -= 1;
+		
+		}
+	}
+}
