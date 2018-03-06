152fd795ebc0d6e931616200d1c6b00d1df0a25d
Hiro <371269@k21schools.org>
2018-03-06 09:06:31 -0900

    Revert "Revert "Revert "Merge branch 'Hiro-dfhaes' of https://github.com/Commodoreprime/somegmae into Hiro-dfhaes"""
    This reverts commit 0fbe78c2cd02985a6fb6a1a277c61105e369f826.

--- projectSandwich/Assets/scripts/Player/PlayerControler.cs
+++ projectSandwich/Assets/scripts/Player/PlayerControler.cs
@@ -44,7 +44,7 @@ public class PlayerControler : MonoBehaviour
 	}
 
 	//Detects a collision
-	void OnTriggerEnter(Collider col)
+	private void OnTriggerEnter(Collider col)
 	{
 		if(col.CompareTag("Enemy"))
 		{
